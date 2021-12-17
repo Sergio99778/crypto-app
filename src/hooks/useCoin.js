bd32198b90f53a8e3c4aa544ff4d592eb5f01062
Sergio99778 <sergio4brandon@gmail.com>
2021-12-17 16:10:53 -0600

    Adding Label and Select (styled components) to useCoin.js

--- src/hooks/useCoin.js
+++ src/hooks/useCoin.js
@@ -1,4 +1,25 @@
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.1rem;
+`;
 
 export const useCoin = (label = 'Options', coinsNames = ['USD']) => {
   const [coin, setCoin] = useState('');
@@ -9,14 +30,14 @@ export const useCoin = (label = 'Options', coinsNames = ['USD']) => {
 
   const SelectCoin = () => (
     <Fragment>
-      <label htmlFor="">{label}</label>
-      <select onChange={handleChange} value={coin} name="" id="">
+      <Label htmlFor="">{label}</Label>
+      <Select onChange={handleChange} value={coin} name="" id="">
         {coinsNames.map((coin) => (
           <option key={coin} value={coin}>
             {coin}
           </option>
         ))}
-      </select>
+      </Select>
     </Fragment>
   );
 
